c08d036e7a973754cc4c22b6c1ecb223ca6c5048
Daniela Rodríguez <danaronait2002@gmail.com>
2023-02-10 12:42:05 +0000

    Commit_014.Animation
    - Falta comentar un script

--- Assets/Script/PowerUp_collider.cs
+++ Assets/Script/PowerUp_collider.cs
@@ -7,7 +7,7 @@ public class PowerUp_collider : MonoBehaviour
     void OnTriggerEnter(Collider player)
     {
         //It compares the tag of that something
-        //If it is a "Power-Up"
+        //If it is a "Player"
         if (player.CompareTag("Player"))
         {
             Destroy(gameObject);
